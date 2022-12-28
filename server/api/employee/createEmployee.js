891c6a1a1a755dbfb1a6148faf1e71f167c652e7
TusharPeoplelink <tushargoyal@peoplelinkvc.com>
2022-12-28 14:36:02 +0530

    update changes

--- server/api/employee/createEmployee.js
+++ server/api/employee/createEmployee.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const employeeModel = require('../../model/employee')
 const divisionModel = require('../../model/division')
 const departmentModel = require('../../model/department')
+const designationModel = require('../../model/designation')
 mongoose.connect('mongodb://localhost:27017/DepartmentEmployeeZoho');
 
 const createEmployee = async (req, res) => {
@@ -17,6 +18,7 @@ const createEmployee = async (req, res) => {
             body : JSON.stringify("Invalid Dept ID")
         }
     }
+    let empDesignation = temp.designation;    
     if(temp.divisionId)
     {
         let checkDivisionID = await divisionModel.findById(temp.divisionId);
@@ -28,7 +30,16 @@ const createEmployee = async (req, res) => {
                 body : JSON.stringify("Invalid Division ID")
             }
         }
-    }    
+    }
+    let findDesignation = await designationModel.findOne({"designationName" : empDesignation})
+    console.log("findDesignation --> ",findDesignation)
+    if(findDesignation == null)
+    {
+        return{
+            statusCode : 404,
+            body : JSON.stringify("Designation does not exist")
+        }
+    }
 
     let data = new employeeModel(temp);
     let result = await data.save();
